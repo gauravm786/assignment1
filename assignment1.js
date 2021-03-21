65ab137860a946afa6c2de88610a47a41f23c1d8
Gaurav Mishra <gauravm.103@gmail.com>
2021-03-21 18:19:25 +0530

    assignment1

---
+++ assignment1.js
@@ -0,0 +1,21 @@
+let min = (array,min=array[0]) =>
+{
+    for(let i =0;i<array.length;i++)
+    {
+    if(min>array[i]) min=array[i];
+    }
+    return min;
+};
+
+let max = (array,max=array[0]) =>
+{
+    for(let i =0;i<array.length;i++)
+    {
+    if(max>array[i]) max=array[i];
+    }
+    return max;
+};
+
+const array =[1,2,3,4,5,0,-1]
+console.log("Minimum value of an array element"+ min(array))
+console.log("Maximum value of an array element"+ max(array))
